d2272a36e0d5f0ef007f770ed408a845db616344
HP <HP@DESKTOP-59GH4OI>
2025-09-13 16:07:04 +0700

    Phần của Chi nè

--- QLTN/chichanhchoe.Designer.cs
+++ QLTN/chichanhchoe.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(469, 80);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 20);
+            this.label1.Size = new System.Drawing.Size(74, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "HELLU";
+            this.label1.Text = "HELLlulu";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // chichanhchoe
