836aae236dad0493f15f2f94fe14eb57351b6c39
HP <HP@DESKTOP-59GH4OI>
2025-10-16 15:20:01 +0700

    resolve: merged frmAdmin designer

--- QLTN/chichanhchoe.Designer.cs
+++ QLTN/chichanhchoe.Designer.cs
@@ -28,31 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.SuspendLayout();
             // 
-            // label1
-            // 
-            
+            // menuStrip1
+           
 
-
-            
             // 
             // button1
             // 
 
-          
+            this.button1.Location = new System.Drawing.Point(588, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 84);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // chichanhchoe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "chichanhchoe";
             this.Text = "chichanhchoe";
+            this.Load += new System.EventHandler(this.chichanhchoe_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,7 +62,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
     }
 }
