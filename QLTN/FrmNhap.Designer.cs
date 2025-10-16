2134910b98b77d5289ac07507f5e6dcf5bba78a0
DELL <DELL@DESKTOP-MTLSMU4>
2025-10-16 14:10:03 +0700

    Phần nháp của Oanh

---
+++ QLTN/FrmNhap.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace QLTN
+{
+    partial class FrmNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(305, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Đây là bản nháp thôi";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FrmNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmNhap";
+            this.Text = "FrmNhap";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
